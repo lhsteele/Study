a8acbe0d58ad63bd9d2f3914872db1950d651e17
Lisa Steele <lisahsteele@gmail.com>
2019-08-07 15:34:30 -0700

    BubbleSort

---
+++ DataStructures/BubbleSort.js
@@ -0,0 +1,25 @@
+/*
+- not efficient
+- not commonly used / useful in production code
+*/
+
+function bubbleSort(arr) {
+  if (!arr.length) return arr;
+
+  let sorted = true;
+
+  while (sorted) {
+    sorted = false
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i]
+        arr[i] = arr[i + 1]
+        arr[i + 1] = temp;
+        sorted = true;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([3, 1, 6, 4, 2, 5, 8, 2]))
