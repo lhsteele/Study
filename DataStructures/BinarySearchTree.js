92dcc545fc71667ce1285b642b45deca942a5df3
Lisa Steele <lisahsteele@gmail.com>
2019-08-08 19:59:26 -0700

    Adding function to BinarySearchTree to check if it's balanced

--- DataStructures/BinarySearchTree.js
+++ DataStructures/BinarySearchTree.js
@@ -28,3 +28,45 @@ function buildBST(array) {
 
   return root;
 }
+
+function isBalanced(root) {
+  let stack = [[root, 0]];
+  let maxHeight = 0;
+  let minHeight = Infinity;
+
+  while (stack.length) {
+    if ((maxHeight - minHeight) > 1) return false;
+
+    let [currentNode, level] = stack.pop();
+    if (currentNode) {
+      // Depth First search 
+      // when to return false? 
+      // when we get to a leaf node or a node with only 1 child
+      if (currentNode.left === null || currentNode.right === null) {
+        if (level > maxHeight) maxHeight = level;
+        if (level < minHeight) minHeight = level;
+      }
+      stack.push([currentNode.left, level + 1])
+      stack.push([currentNode.right, level + 1])
+    }
+  }
+  console.log(`maxHeight: ${maxHeight}`)
+  console.log(`minHeight: ${minHeight}`)
+  return true;
+}
+
+let balancedTree = buildBST([1, 3, 5, 8, 19, 23, 24, 31])
+let unbalancedTree = new TreeNode(12)
+unbalancedTree.left = new TreeNode(8)
+unbalancedTree.left.left = new TreeNode(5)
+unbalancedTree.left.right = new TreeNode(9)
+unbalancedTree.left.left.left = new TreeNode(1)
+unbalancedTree.left.left.left.left = new TreeNode(0)
+unbalancedTree.right = new TreeNode(14)
+unbalancedTree.right.right = new TreeNode(18)
+
+
+console.log(isBalanced(balancedTree)) 
+// should return true
+console.log(isBalanced(unbalancedTree))
+// should return false
