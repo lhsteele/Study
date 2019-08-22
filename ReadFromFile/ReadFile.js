c27d8342eb4256d760518cb0bde38f456a312f91
Lisa Steele <lisahsteele@gmail.com>
2019-08-22 15:34:33 -0700

    Reading a CSV and JSON file

--- ReadFromFile/ReadFile.js
+++ ReadFromFile/ReadFile.js
@@ -2,11 +2,31 @@ var fs = require('fs');
 var http = require('http');
 
 // read from text file and write a new one
-fs.readFile('myCSV.txt', 'utf8', function(err, data) {
-  fs.writeFile('csvWrite.txt', data, function(err, data) {
+fs.readFile('myTxt.txt', 'utf8', function(err, data) {
+  // console.log(data)
+  fs.writeFile('txtWrite.txt', data, function(err, data) {
     if (err) console.log("error", err)
   })
 })
+
+// read from a json file
+fs.readFile('myJSON.json', 'utf8', function(err, data) {
+  let players = JSON.parse(data);
+  for (player in players) {
+    // console.log(players[player].Player)
+  }
+})
+
+// read from a CSV file
+fs.readFile('myCSV.csv', 'utf8', function(err, data) {
+  let csvData = data;
+  
+  csvData.split('\n').forEach((entry, i) => {
+    console.log(`${i + 1}:${entry}`);
+    
+  })
+})
+
 
 // write to a webpage
 http.createServer(function (req, res) {
